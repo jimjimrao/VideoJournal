67502de506fcc3af9ecb19460c90f1e3f02262d5
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-22 20:06:01 -0500

    Initial Commit

---
+++ VideoJournal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoJournal
+//
+//  Created by Jimmy Rao on 11/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoJournal/VideoJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoJournalApp.swift
+//  VideoJournal
+//
+//  Created by Jimmy Rao on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
