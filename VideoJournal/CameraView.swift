23d8aec7c0f0cd732e0c07ca19b871ad69a98dcf
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-27 19:18:21 -0500

    initial commit: CameraView

---
+++ VideoJournal/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  VideoJournal
+//
+//  Created by Jimmy Rao on 11/27/23.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
