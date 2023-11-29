879bdbc941f7e30933c5483785d4cd52f785059d
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-28 20:53:01 -0500

    feat: save button and flip camera button

--- VideoJournal/CameraView.swift
+++ VideoJournal/CameraView.swift
@@ -15,31 +15,70 @@ struct ContentView: View {
 
 
 struct CameraView: View {
+    @StateObject var camera = CameraModel()
     var body: some View {
         ZStack{
             Color.black
                 .ignoresSafeArea(.all, edges: .all)
             
             VStack{
-                
+                if camera.isTaken{
+                    HStack {
+                        Spacer()
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Image(systemName: 
+                                "arrow.triangle.2.circlepath.camera")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(Color.white)
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            }
+                        )
+                        .padding(.trailing, 10)
+                    }
+                }
                 Spacer()
                 
                 HStack{
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        ZStack{
-                            Circle()
-                                .fill(Color.white)
-                                .frame(width: 65, height: 65)
-                            
-                            Circle()
-                                .stroke(Color.white, lineWidth:2)
-                                .frame(width: 75, height: 75)
-                        }
-                    })
+//                    if taken showing save and again take button...
+                    if camera.isTaken{
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Save")
+                                .foregroundColor(.black)
+                                .fontWeight(.semibold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 20)
+                                .background(Color.white)
+                                .clipShape(Capsule())
+                            }
+                        )
+                        .padding(.leading)
+                        Spacer()
+                        
+                    }else{
+                        Button(action: {camera.isTaken.toggle()}, label: {
+                            ZStack{
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 65, height: 65)
+                                
+                                Circle()
+                                    .stroke(Color.white, lineWidth:2)
+                                    .frame(width: 75, height: 75)
+                                }
+                            }
+                        )
+                    }
                 }
+                .frame(height: 75)
             }
         }
     }
+}
+ 
+// Camera Model
+class CameraModel: ObservableObject {
+    @Published var isTaken = false
 }
 
 #Preview {
