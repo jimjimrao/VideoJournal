3ef51390fdfd25f2e3c55f67cf191c51f7fe32c8
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-28 20:11:10 -0500

    feat: shutter button

--- VideoJournal/CameraView.swift
+++ VideoJournal/CameraView.swift
@@ -2,17 +2,46 @@
 //  CameraView.swift
 //  VideoJournal
 //
-//  Created by Jimmy Rao on 11/27/23.
+//  Created by Jimmy Rao on 11/28/23.
 //
 
 import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        CameraView()
+    }
+}
+
+
 struct CameraView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.black
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack{
+                
+                Spacer()
+                
+                HStack{
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        ZStack{
+                            Circle()
+                                .fill(Color.white)
+                                .frame(width: 65, height: 65)
+                            
+                            Circle()
+                                .stroke(Color.white, lineWidth:2)
+                                .frame(width: 75, height: 75)
+                        }
+                    })
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    CameraView()
+    ContentView()
 }
--- VideoJournal/VideoJournalApp.swift
+++ VideoJournal/VideoJournalApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct VideoJournalApp: App {
     var body: some Scene {
         WindowGroup {
-            SuccessView()
+            CameraView()
         }
     }
 }
