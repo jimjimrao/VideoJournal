71f941aff5d0da52bcaee3c5b9b49744c53ae1e0
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-12-14 18:50:24 -0500

    chore: remove isTaken toggle

--- VideoJournal/VideoContentView.swift
+++ VideoJournal/VideoContentView.swift
@@ -100,7 +100,6 @@ struct VideoContentView: View {
                                 }
                             case .photo:
                                 viewModel.aespaSession.capturePhoto(autoVideoOrientationEnabled: true)
-                                viewModel.isTaken.toggle()
                             }
                         }
                         Spacer()
